5c14e787709c8811f15ce734349354cc7deee511
khornSaokhouch <khornsaokhouch4456@gmail.com>
2026-07-07 14:41:44 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,27 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com",
+              "frame-src 'self' https://challenges.cloudflare.com",
+              "style-src 'self' 'unsafe-inline' https://challenges.cloudflare.com",
+              "font-src 'self' https://challenges.cloudflare.com",
+              "connect-src 'self' https://challenges.cloudflare.com",
+              "img-src 'self' data: blob: https://ik.imagekit.io https://challenges.cloudflare.com",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
